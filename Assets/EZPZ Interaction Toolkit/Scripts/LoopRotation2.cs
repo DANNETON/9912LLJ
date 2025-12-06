b4a34c4dcfd3b8876b9b91b8a8bbf94551593b6c
GNE\GE <G_Ellitz@outlook.com>
2025-12-07 02:47:19 +1100

    Assess2-2

---
+++ Assets/EZPZ Interaction Toolkit/Scripts/LoopRotation2.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+using System.Numerics;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class LoopRotation2 : MonoBehaviour
+{
+    public Vector3 minAngle = new Vector3();
+    public Vector3 maxAngle = new Vector3();
+    public bool mirror = false;
+
+    public void OnEnable()
+    {
+        
+    }
+
+    private void Start()
+    {
+        //Reset();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    public void Reset()
+    {
+        
+    }
+
+    public void DealProgress(float progress)
+    {
+        if (mirror)
+        {
+            progress = Mathf.PingPong(progress, 1);
+        }
+        progress = Mathf.Clamp01(progress % 1);
+
+        float x = Mathf.Lerp(minAngle.x, maxAngle.x, progress);
+        float y = Mathf.Lerp(minAngle.y, maxAngle.y, progress);
+        float z = Mathf.Lerp(minAngle.z, maxAngle.z, progress);
+
+        transform.localRotation = Quaternion.Euler(x, y, z);
+    }
+}
